0b8c95b2ca0fdb91387d314345106c20bfee503e
Sjaak Priester <sjaak@sjaakpriester.nl>
2024-06-29 14:48:03 +0200

    fetch mode

--- assets/linktitles.js
+++ assets/linktitles.js
@@ -1,10 +1,10 @@
 function linktitles(selector, action) {
-    document.querySelectorAll(selector).forEach((elmt) => {
-        elmt.querySelectorAll('a:not([href*="//"]):not([title])').forEach((link) => {
+    document.querySelectorAll(selector).forEach(elmt => {
+        elmt.querySelectorAll('a:not([href*="//"]):not([title])').forEach(link => {
             const url = link.getAttribute('href'),
                 idx = url.lastIndexOf('/');
             if (idx > -1)   {
-                fetch(url.slice(0, idx) + '/' + action + url.slice(idx))
+                fetch(url.slice(0, idx) + '/' + action + url.slice(idx), {mode: 'same-origin'})
                     .then(resp => resp.json())
                     .then(d => { if (d.title) link.title = d.title; });
             }
